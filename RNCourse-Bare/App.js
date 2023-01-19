102217dd7a62a418b6bf93d2941d0e3a80f21523
Arkadi Berikashvili <arkadi.berikashvili@valor-software.com>
2023-01-19 23:07:46 +0400

    bare workflow - location permissions on ios and android

--- RNCourse-Bare/App.js
+++ RNCourse-Bare/App.js
@@ -1,13 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import * as Location from 'expo-location';
+
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [errorMsg, setErrorMsg] = useState();
+  const [currentLocation, setCurrentLocation] = useState();
+
+  const getUserLocationHandler = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+
+    if (status !== 'granted') {
+      setErrorMsg('Permission to access location was denied');
+      return;
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+
+    console.log('LOCATION', location);
+
+    const { latitude, longitude } = location.coords;
+
+    setCurrentLocation({ latitude, longitude });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        {currentLocation && (
+          <View>
+            <Text>
+              Your location is: {currentLocation.latitude}, {currentLocation.longitude}
+            </Text>
+          </View>
+        )}
+
+        {errorMsg && <Text style={styles.error}>{errorMsg}</Text>}
+
+        <Button title="Get Location" onPress={getUserLocationHandler} />
+      </View>
+    </>
   );
 }
 
@@ -16,6 +50,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  error: {
+    fontSize: 18,
+    color: 'red',
+    textAlign: 'center'
+  }
 });

b0d149d6872e6fa8501ad6955f350b5544ef8b73
Arkadi Berikashvili <arkadi.berikashvili@valor-software.com>
2023-01-20 02:03:54 +0400

    push notifications

--- RNCourse-PushNotifications/App.js
+++ RNCourse-PushNotifications/App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import * as Notifications from 'expo-notifications';
 
-import { Button, StyleSheet, View } from 'react-native';
+import { Alert, Button, Platform, StyleSheet, View } from 'react-native';
 import { useEffect } from 'react';
 
 Notifications.setNotificationHandler({
@@ -16,14 +16,46 @@ Notifications.setNotificationHandler({
 
 export default function App() {
   useEffect(() => {
+    const configurePushNotifications = async () => {
+      const { status } = await Notifications.getPermissionsAsync();
+
+      let finalStatus = status;
+
+      if (finalStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+
+        finalStatus = status;
+      }
+
+      if (finalStatus !== 'granted') {
+        Alert.alert('Permission required', 'Push notification needs appropriate permissions...');
+
+        return;
+      }
+
+      const pushTokenData = await Notifications.getExpoPushTokenAsync();
+      console.log(pushTokenData);
+
+      if (Platform.OS === 'android') {
+        Notifications.setNotificationChannelAsync('default', {
+          name: 'default',
+          importance: Notifications.AndroidImportance.DEFAULT
+        });
+      }
+    };
+
+    configurePushNotifications();
+  }, []);
+
+  useEffect(() => {
     const sub1 = Notifications.addNotificationReceivedListener((notification) => {
-      console.log('Notification received', notification);
+      // console.log('Notification received', notification);
       const { userName } = notification.request.content.data;
-      console.log('Received UserName', userName);
+      // console.log('Received UserName', userName);
     });
 
     const sub2 = Notifications.addNotificationResponseReceivedListener((response) => {
-      console.log('Notification Response Received', response);
+      // console.log('Notification Response Received', response);
       const { userName } = response.notification.request.content.data;
       console.log('Received UserName from Response', userName);
     });
@@ -47,9 +79,24 @@ export default function App() {
     });
   };
 
+  const sendPushNotificationHandler = () => {
+    fetch('https://exp.host/--/api/v2/push/send', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        to: 'ExponentPushToken[ns6qScBjWhE3RnNrr7duQ_]',
+        title: 'Test - sent from device!',
+        body: 'This is test!'
+      })
+    });
+  };
+
   return (
     <View style={styles.container}>
       <Button title="Schedule Notification" onPress={scheduleNotificationHandler} />
+      <Button title="Send Push Notification" onPress={sendPushNotificationHandler} />
       <StatusBar style="auto" />
     </View>
   );
